34ad938ae48e4df576f586714cab45d53886fe51
karimzade_k <karimzade_k@STHQ0118-01>
2025-08-30 20:53:32 +0400

    program.cs-de deyisiklik

--- Lesson_4_EntityFrameworkCodeFirst/Program.cs
+++ Lesson_4_EntityFrameworkCodeFirst/Program.cs
@@ -47,46 +47,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        var context = new AppDbContext();
-
+       var context = new AppDbContext();
+        //
         // context.Categories.Add(new Category()
         // {
-        //     Name = "Sud mehsullari",
+        //     Name = "Sud",
         //     Description = "Umumen nese 1 sey"
         // });
-        // 
-        // context.Products.Add(new Product()
+        // context.SaveChanges();
+        //
+        // for (int i = 0; i < 100; i++)
         // {
-        //     Name = "Yogurt",
-        //     UnitPrice = 99.9,
-        //     UnitInStock = 50,
-        //     CategoryId = 1
-        // });
+        //     context.Products.Add(new Product()
+        //     {
+        //         Name = $"Yogurt {i}",
+        //         UnitPrice = Random.Shared.Next(1, 5000),
+        //         UnitInStock = Random.Shared.Next(1, 1000),
+        //         CategoryId = 5
+        //     });
+        // }
+        //
+        // //context.Products.Add(new Product()
+        // //{
+        // //    Name = "Yogurt",
+        // //    UnitPrice = 99.9,
+        // //    UnitInStock = 50,
+        // //    CategoryId = 1
+        // //});
+        //
+        //
+        // Console.WriteLine("Successfully added Product");
+        // context.SaveChanges();
 
 
-        Console.WriteLine("Successfully added Product");
+        // var list = context.Products
+        //                             .Where(x => x.Id == 15)
+        //                             .Select(x => new GetProductResponseModel
+        //                             {
+        //                                 Name = x.Name,
+        //                                 UnitPice = x.UnitPrice
+        //                             })
+        //                             .ToList();
+        // 
+        // foreach (var item in list)
+        // {
+        //     Console.WriteLine($"{item.Name} - {item.UnitPice}");
+        // }
 
 
-        var list = context.Products
-                                    .Where(x => x.Id == 3)
-                                    .Select(x => new GetProductResponseModel
-                                    {
-                                        Name = x.Name,
-                                        UnitPice = x.UnitPrice
-                                    })
-                                    .ToList();
-
-        foreach (var item in list)
-        {
-            Console.WriteLine($"{item.Name} - {item.UnitPice}");
-        }
+        var selectedProduct = context.Products
+                                .Select(x=>new GetProductResponseModel
+                                {
+                                    Id = x.Id,
+                                    Name = x.Name,
+                                    UnitPrice = x.UnitPrice
+                                })
+                                .FirstOrDefault(x => x.Id == 6);
 
 
-        var productOne = context.Products.FirstOrDefault(x => x.Id == 1);
 
-        productOne.Name = "Kelem dolmasi";
-
-        context.SaveChanges();
-
+        Console.WriteLine($"{selectedProduct.Name} - {selectedProduct.UnitPrice}");
     }
 }
--- Lesson_4_EntityFrameworkCodeFirst/ResponseModel/GetProductResponseModel.cs
+++ Lesson_4_EntityFrameworkCodeFirst/ResponseModel/GetProductResponseModel.cs
@@ -2,6 +2,7 @@
 
 public class GetProductResponseModel
 {
+    public int Id { get; set; }
     public string Name { get; set; }
-    public double UnitPice { get; set; }
+    public double UnitPrice { get; set; }
 }
